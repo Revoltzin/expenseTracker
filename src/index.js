8f395fc39c69da8e31f7d0eeb94a0a5a374ba3aa
Revoltzin <jat1963@gmail.com>
2026-01-20 21:54:52 -0300

    Expenses routes added

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ const express = require("express")
 
 const app = express()
 
+const expensesRoutes = require("./expenses.routes.js")
 const routes = require("./routes.js")
 
 app.use(express.json())
 app.use(routes)
+app.use(expensesRoutes)
 
 app.listen(3000, () => {
     console.log("Server is running")
