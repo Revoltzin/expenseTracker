cd4f2b76b78b29800d3ff1a26d5247216235509d
Revoltzin <jat1963@gmail.com>
2026-01-20 22:10:35 -0300

    FindByEmail created into the repository

--- src/app/repository/ExpenseRepository.js
+++ src/app/repository/ExpenseRepository.js
@@ -1,8 +1,11 @@
+const pool = require('../../database/index.js')
 const bcrypt = require("bcrypt")
 
 class ExpenseRepository {
     async findByEmail (email) {
-
+        const query = 'SELECT * FROM users WHERE email = $1'
+        const result = await pool.query(query, [email])
+        return result.rows[0] || null
     }
 }
 
