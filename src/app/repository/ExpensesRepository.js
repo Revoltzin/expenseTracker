559b941a9446b9448991bd533768cad041123e4d
Revoltzin <jat1963@gmail.com>
2026-01-22 19:54:04 -0300

    Create function added

--- src/app/repository/ExpensesRepository.js
+++ src/app/repository/ExpensesRepository.js
@@ -1,5 +1,15 @@
+const pool = require("../../database/index.js")
 class ExpensesRepository {
+    async create ({ userId, title, description, price }) {
+        const query = `
+        INSERT INTO expenses (user_id, title, description, price)
+        VALUES ($1, $2, $3, $4)
+        RETURNING *
+        `
 
+        const result = await pool.query(query, [userId, title, description, price])
+        return result.rows[0]
+    }
 }
 
 module.exports = new ExpensesRepository()
