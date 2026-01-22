55376f099f11eb0dc742599e0b043f96a6bab357
Revoltzin <jat1963@gmail.com>
2026-01-22 20:19:56 -0300

    Create function created into ExpenseController

--- src/app/controller/ExpensesController.js
+++ src/app/controller/ExpensesController.js
@@ -6,7 +6,18 @@ class ExpensesController {
         const expenses = await ExpensesRepository.findAllByUser(userId)
         return res.json(expenses)
     }
-    
+
+    async create (req, res) {
+        const userId = req.user.userId
+        const { title, description, price } = req.body
+
+        if (!title) {
+            return res.status(400).json({ error: "Title is required"})
+        }
+
+        const expense = await ExpensesRepository.create({ userId, title, description, price })
+        return res.status(201).json(expense)
+    }
 }
 
 module.exports = new ExpensesController()
