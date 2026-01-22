380fd39c7b35f6c7977bde736eb02d899d8b8e59
Revoltzin <jat1963@gmail.com>
2026-01-22 20:16:57 -0300

    Index feature created into ExpensesController

--- src/app/controller/ExpensesController.js
+++ src/app/controller/ExpensesController.js
@@ -1,5 +1,12 @@
-class ExpensesController {
+const ExpensesRepository = require("../repository/ExpensesRepository.js")
 
+class ExpensesController {
+    async index (req, res) {
+        const userId = req.user.userId
+        const expenses = await ExpensesRepository.findAllByUser(userId)
+        return res.json(expenses)
+    }
+    
 }
 
 module.exports = new ExpensesController()
